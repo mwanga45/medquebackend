20cde099b763bfec39a7856b9dcd3daa36d9eb16
Lynxprazo <issamwanga02@gmail.com>
2025-03-12 15:16:23 +0300

    more changes

--- db_conn/dbconnection.go
+++ db_conn/dbconnection.go
@@ -8,14 +8,15 @@ import (
 )
 
 var Db *sql.DB
+
 // initilaze connection pool
 func Connectionpool(databasesourceName string) error {
 	var err error
-	if Db,err = sql.Open("postgres", databasesourceName);err !=nil{
+	if Db, err = sql.Open("postgres", databasesourceName); err != nil {
 		return err
 	}
-	// Db.Ping used to verify if the connection is alive and properly configured 
-	if err = Db.Ping(); err != nil{
+	// Db.Ping used to verify if the connection is alive and properly configured
+	if err = Db.Ping(); err != nil {
 		return err
 	}
 
@@ -32,20 +33,32 @@ func Connectionpool(databasesourceName string) error {
     consultation_fee DECIMAL(10,2),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-`
-	if _,err = Db.Exec(doctor_tb);err !=nil{
+` 
+	if _, err = Db.Exec(doctor_tb); err != nil {
 		log.Fatalf("failed to create new table %v", err)
+	}
+	patient_tb := `CREATE TABLE IF NOT EXISTS users (
+		user_id INT PRIMARY KEY AUTO_INCREMENT,
+		full_name VARCHAR(150) NOT NULL,
+		home_address VARCHAR(150),
+		email VARCHAR(100) UNIQUE NOT NULL,
+		phone_number VARCHAR(20) UNIQUE,
+		user_type VARCHAR(20) CHECK (user_type IN ('Patient')),
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);`
+	if _,err = Db.Exec(patient_tb);err !=nil{
+		log.Fatalf("failed to create table patient_tb %v", err)
 	}
 
 	return nil
 
 }
-// create fi=unction to terminate connection 
-func Closeconn()error {
-	if Db != nil{
+
+// create function to terminate connection
+func Closeconn() error {
+	if Db != nil {
 		return Db.Close()
 	}
 	return nil
 
-	
 }
