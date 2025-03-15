22499ceed848d9ab2bb891e37d10d5955d4f0306
Lynxprazo <issamwanga02@gmail.com>
2025-03-15 12:37:03 +0300

    more changes

--- api/api.go
+++ api/api.go
@@ -1,12 +1,24 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
-func doctors(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet{
-		http.Error(w,"Invalid methode used", http.StatusBadRequest)
-      return
-	}
-	query := "SELECT * FROM doctors"
+type Response struct {
+	Messsage string      `json:"message"`
+	Success  bool        `json:"success,omitempty"`
+	Data     interface{} `json:"Data" `
+}
+
+func Doctors(w http.ResponseWriter, r *http.Request) {
+   if r.Method != http.MethodGet{
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	json.NewEncoder(w).Encode(Response{
+		Messsage: "Invalid method used ",
+		Success: false,
+	})
+	return
+   }
 
 }
