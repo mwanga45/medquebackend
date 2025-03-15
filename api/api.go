7b3607f607b66306c9ebf07a4f6e5b8a9fb9b1a8
Lynxprazo <issamwanga02@gmail.com>
2025-03-15 20:42:17 +0300

    more changes

--- api/api.go
+++ api/api.go
@@ -34,11 +34,14 @@ func Doctors(w http.ResponseWriter, r *http.Request) {
 	}
 	// create are slice which will hold the row as value and column name as key at end
 	var doctors []map[string]interface{}
+	// store columns name variable
 	columns, _ := rows.Columns()
 	count := len(columns)
+	// create slice for store temporarly  column as key and data as value with corresponding data type of column
 	value := make([]interface{}, count)
+	// create  slice pointer neccessary during scan value data to corresponding column name
 	valueptrs := make([]interface{}, count)
-
+	// automatic move cursor to next rows
 	for rows.Next() {
 		for i := range columns {
 			valueptrs[i] = &value[i]
@@ -52,8 +55,7 @@ func Doctors(w http.ResponseWriter, r *http.Request) {
 		}
 		doctors = append(doctors, doctor)
 	}
-
-
+	// handling rows error
 	if err := rows.Err(); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(Response{
@@ -62,13 +64,14 @@ func Doctors(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
+
 	w.Header().Set("content-type", "application/json")
 	err = json.NewEncoder(w).Encode(Response{
 		Messsage: "successfuly fetch data",
 		Success:  true,
 		Data:     doctors,
 	})
-
+	// handling encoding process if error occur
 	if err != nil {
 		json.NewEncoder(w).Encode(Response{
 			Messsage: "failed to encode data ",
