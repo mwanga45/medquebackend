fe2b0cea9c8c7538d0e2968b4bf24f96f02915cd
Lynxprazo <issamwanga02@gmail.com>
2025-03-21 14:59:07 +0300

    more changes

--- chatbot/chat.go
+++ chatbot/chat.go
@@ -4,34 +4,47 @@ import (
 	"encoding/json"
 	"net/http"
 )
-//create struct that will hold text field
-type Part struct{
-	Text string `json:"text"`
-}
-// create that will hold two field   that role will be either user or model and another field will be slice of content 
-type Content struct{
-	Role string `json:"role"`
-	Parts []Part `json:"parts"`
-}
-// create struct that will hold field to configure the userInput  in property 
-type GenerateConfig struct{
-	Temperature float32 `json:"temperature"`
-	TopK int`json:"topk"`
-	TopP  float32 `json:"topP"`
-	MaxOutputToken int `json:"maxoutputtoken"`
-}
-// 
-type chatbotResponse struct{
-	Response string `json:"response"`
-	MessageResonseError string `json:"messageResponseError,omitempty"`
-}
+
+// create struct that will hold text field
+type (
+	Part struct {
+		Text string `json:"text"`
+	}
+	// create that will hold two field   that role will be either user or model and another field will be slice of content
+	Content struct {
+		Role  string `json:"role"`
+		Parts []Part `json:"parts"`
+	}
+	// create struct that will hold field to configure the userInput  in property
+	GenerateConfig struct {
+		Temperature    float32 `json:"temperature"`
+		TopK           int     `json:"topk"`
+		TopP           float32 `json:"topP"`
+		MaxOutputToken int     `json:"maxoutputtoken"`
+	}
+	// create the struct  will check the safety of message
+	SafetySetting struct {
+		Category  string `json:"category"`
+		Threshold string `json:"threshold"`
+	}
+	// create struct that will able to generate request
+	GenerateRequest struct{
+		Contents []Content `json:"contents"`
+
+	}
+	chatbotResponse struct {
+		Response            string `json:"response"`
+		MessageResonseError string `json:"messageResponseError,omitempty"`
+	}
+
+)
 
 func Chatbot() {
 
 }
 
-func SendErr(w http.ResponseWriter, message string, statusCode int){
-	w.Header().Set("Content-Type","application/json")
+func SendErr(w http.ResponseWriter, message string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(chatbotResponse{
 		MessageResonseError: message,
 	})
