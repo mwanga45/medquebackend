ea167974c653b5b2789fe519c3776faf5adf40a0
Lynxprazo <issamwanga02@gmail.com>
2025-04-02 10:14:52 +0300

    morechanges

--- api-web/webapihandler.go
+++ api-web/webapihandler.go
@@ -1,9 +1,12 @@
 package apiweb
-
+ type Home_address struct{
+	City string `json:"city,omitempty"`
+	PostAdddres string `json:"post_address,omitempty"`
+ }
 type Staffdetatails struct {
  Username  string `json:"username"`
  Password string `json:"password"`
  Phone string `json:"phone"`
  Email string `json:"email"`
- 
+ Home_address Home_address `json:"home_address"`
 }
