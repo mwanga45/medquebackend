60156ac9b711feeb4e880805cf2e96a63e39d83c
Lynxprazo <issamwanga02@gmail.com>
2025-04-02 10:10:33 +0300

    morechanges

---
+++ api-web/webapihandler.go
@@ -0,0 +1,9 @@
+package apiweb
+
+type Staffdetatails struct {
+ Username  string `json:"username"`
+ Password string `json:"password"`
+ Phone string `json:"phone"`
+ Email string `json:"email"`
+ 
+}
