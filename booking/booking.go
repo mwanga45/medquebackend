7f87e1495b109c100fe9c947195a9987e9a03295
Lynxprazo <issamwanga02@gmail.com>
2025-04-05 09:05:18 +0300

    more changes

--- booking/booking.go
+++ booking/booking.go
@@ -1,12 +1,20 @@
 package booking
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type (
 	Respond struct {
 		Message string      `json:"message"`
 		Success bool        `json:"success"`
 		Data    interface{} `json:"data"`
+	}
+	BookingRequest struct {
+		Time       int64  `json:"time" validate:"required"`
+		Department string `json:"department" validate:"required"`
+		Day        string `json:"day" validate:"required"`
+		Diseases   string `json:"desease" validate:"required"`
 	}
 )
 
