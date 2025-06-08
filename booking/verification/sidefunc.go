0c5cf268d516113fa5a3864f356981c36b5346f1
mwanga45 <issamwanga02@gmail.com>
2025-06-08 12:45:18 +0300

    more changes

--- booking/verification/sidefunc.go
+++ booking/verification/sidefunc.go
@@ -1,5 +1,44 @@
 package sidefunc_test
 
-func GenerateTimeSlote(timeInterval int, startTime int, endTime int){
+import (
+	"fmt"
+	"time"
+)
+
+type Timeslot struct{
+	StartTime  string
+	EndTime string
+}
+func GenerateTimeSlote(timeInterval int, startTime string, endTime string)([]Timeslot, error) {
+	layout := "15:04"
+	now := time.Now()
+
+	start, err :=  time.ParseInLocation(layout,startTime,now.Location())
+	if err != nil{
 	
+		return nil , fmt.Errorf("Something went wrong", err)
+	}
+   end, err :=  time.ParseInLocation(layout, endTime, now.Location())
+   if err != nil{
+	return nil , fmt.Errorf("Something  went wrong ", err)
+   }
+   if end.Before(start){
+	  end = end.Add(24 *time.Hour)
+   }
+   var slot []Timeslot
+   current  :=  start
+
+   for {
+	next :=  current.Add(time.Duration(timeInterval) *time.Minute)
+	if current.After(end){
+		break
+	}
+	slot = append(slot, Timeslot{
+		StartTime: current.Format(layout),
+		EndTime: next.Format(layout),
+	})
+	current = next
+   }
+   return  slot , nil
+
 }
