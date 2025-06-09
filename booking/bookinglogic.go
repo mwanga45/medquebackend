819d0965840e1ce92b9b4f399369ca15c34f891f
mwanga45 <issamwanga02@gmail.com>
2025-06-09 20:09:34 +0300

    more changes

--- booking/bookinglogic.go
+++ booking/bookinglogic.go
@@ -2,6 +2,7 @@ package booking
 
 import (
 	"encoding/json"
+	"fmt"
 	handlerconn "medquemod/db_conn"
 	"net/http"
 )
@@ -53,7 +54,27 @@ func bookinglogic(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	 }
-	 checklist  := tx.QueryRow("")
-	 
+	 if err :=  json.NewDecoder(r.Body).Decode(&bsr)
+	row := tx.QueryRow(` SELECT dk.doctor_id, dk.doctorname,ds.day_of_week,ds.start_time, ds.end_time,sa.servicename,sa.duration_minutes,sa.fee FROM doctors AS dk JOIN doctor_services AS ds ON dk.doctor_id = ds.doctor_id JOIN serviceAvailable AS sa ON ds.service_id = sa.serv_id WHERE sa.servicename = $1`, bsr.ServiceName) 
+
+err := row.Scan(
+    &bsr.DoctorID,
+    &bsr.DoctorName,
+    &bsr.DayOfWeek,
+    &bsr.StartTime,
+    &bsr.EndTime,
+    &bsr.ServiceName,
+    &bsr.DurationMinutes,
+    &bsr.Fee,
+)
+if err != nil {
+	w.WriteHeader(http.StatusInternalServerError)
+    json.NewEncoder(w).Encode(Response{
+		Message: "InternaserverError ",
+		Success: false,
+	})
+    return fmt.Errorf("Soemthing went wrong", err)
+}
+
 
 }
