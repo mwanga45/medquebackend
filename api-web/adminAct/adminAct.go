8c41d30c0c963b21e3872d8091dd13aad629ad8e
mwanga45 <issamwanga02@gmail.com>
2025-06-15 22:21:10 +0300

    more changes

--- api-web/adminAct/adminAct.go
+++ api-web/adminAct/adminAct.go
@@ -5,13 +5,25 @@ import (
 	"net/http"
 )
 type (
-	ServAssignpayload{
-		
+	ServAssignpayload struct{
+		Servname string `json:"servname"`
+		DurationMin  int `json:"duration_time"`
+		Fee float64 `json:"fee"`
+	}
+	Response struct{
+		Message  string `json:"message"`
+		Success bool `json:"success"`
 	}
 )
 
 func AssignService(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost{
-		json.NewEncoder()
+		json.NewEncoder(w).Encode(Response{
+			Message: "Invalidpayload",
+			Success: false,
+		})
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+	w.Header().Set("Content-Type", "application/json")
 }
